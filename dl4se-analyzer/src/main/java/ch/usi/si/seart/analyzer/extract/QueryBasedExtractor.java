0b38274c4ca938e0d52b4a56df3fd45dc60974c6
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-07-28 15:34:57 +0000

    Update classes

--- dl4se-analyzer/src/main/java/ch/usi/si/seart/analyzer/extract/QueryBasedExtractor.java
+++ dl4se-analyzer/src/main/java/ch/usi/si/seart/analyzer/extract/QueryBasedExtractor.java
@@ -35,4 +35,25 @@ public abstract class QueryBasedExtractor implements Extractor {
                 .map(Capture::getName)
                 .collect(Collectors.toUnmodifiableSet());
         if (!captures.contains("target"))
-      
+            throw new IllegalStateException("Query must have a 'target' capture defined!");
+        if (captures.size() > 1 && !captures.contains("additional"))
+            throw new IllegalStateException("Only an 'additional' capture is supported!");
+    }
+
+    @Override
+    public List<Match> extract(Tree tree) {
+        @Cleanup Query query = Query.builder()
+                .language(getLanguage())
+                .patterns(getPatterns())
+                .build();
+        validate(query);
+        Node node = getStartingNode(tree);
+        @Cleanup QueryCursor cursor = node.walk(query);
+        Stream<QueryMatch> matches = StreamSupport.stream(cursor.spliterator(), false);
+        return matches.map(match -> {
+            Node target = match.getNodes("target").stream().findFirst().orElseThrow();
+            List<Node> additional = match.getNodes("additional").stream().collect(Collectors.toUnmodifiableList());
+            return new Match(target, additional);
+        }).collect(Collectors.toUnmodifiableList());
+    }
+}
