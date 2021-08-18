e64a26760d3a52453e769fa99c1cf371a0560283
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-08-18 21:41:10 +0000

    Implement

---
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/config/properties/SchedulingProperties.java
@@ -0,0 +1,11 @@
+package ch.usi.si.seart.server.config.properties;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.scheduling.support.CronTrigger;
+
+import javax.validation.constr
