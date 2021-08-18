3921cc2c9b14f904aafdebce07bd4338b4fa539b
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-08-18 22:39:22 +0000

    DONE MUST TEST CELERY TASK  QUEUE

--- dl4se-server/src/main/java/ch/usi/si/seart/server/controller/LanguageController.java
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/controller/LanguageController.java
@@ -1,26 +1,32 @@
 package ch.usi.si.seart.server.controller;
 
-import ch.usi.si.seart.server.service.CodeService;
+import ch.usi.si.seart.model.Language;
+import ch.usi.si.seart.service.LanguageService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("/code")
+@RequestMapping("/language")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @AllArgsConstructor(onConstructor_ = @Autowired)
-public class CodeController {
+public class LanguageController {
 
-    CodeService codeService;
+    LanguageService languageService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> code(@PathVariable Long id) {
-        return ResponseEntity.ok(codeService.getWithId(id));
+    @GetMapping
+    public ResponseEntity<?> languages() {
+        List<Language> languages = languageService.getAll();
+        return ResponseEntity.ok(
+                languages.stream().map(Language::getName).collect(Collectors.toList())
+        );
     }
 }
