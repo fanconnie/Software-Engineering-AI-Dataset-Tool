3c69da0d71e8d9f2b4ca6facccb7d987ad9c4b08
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-12-09 10:32:15 +0000

    Update quote2.xml

--- dl4se-transformer/src/test/java/ch/usi/si/seart/transformer/remover/PythonDocstringRemoverTest.java
+++ dl4se-transformer/src/test/java/ch/usi/si/seart/transformer/remover/PythonDocstringRemoverTest.java
@@ -7,4 +7,38 @@ class PythonDocstringRemoverTest extends PythonBaseTest {
 
     @Override
     protected Transformer getTestSubject() {
-        return new
+        return new PythonDocstringRemover();
+    }
+
+    @Override
+    protected String getTestInput() {
+        return "\"\"\"module docstring\"\"\"\n" +
+                "\n" +
+                "# comment\n" +
+                "def func(x): # comment\n" +
+                "    \"\"\"Function docstring\"\"\"\n" +
+                "    pass # comment\n" +
+                "    \n" +
+                "class Example:\n" +
+                "    # comment\n" +
+                "    # comment\n" +
+                "    \"\"\"Class docstring\"\"\"\n" +
+                "    pass\n";
+    }
+
+    @Override
+    protected String getExpectedOutput() {
+        return "\n" +
+                "\n" +
+                "# comment\n" +
+                "def func(x): # comment\n" +
+                "    \n" +
+                "    pass # comment\n" +
+                "    \n" +
+                "class Example:\n" +
+                "    # comment\n" +
+                "    # comment\n" +
+                "    \n" +
+                "    pass\n";
+    }
+}
